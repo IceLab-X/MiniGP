e58e2d0aa7c3bcf7e647e08dde24a467f8240364
ZhenjieLu <89203680+Fillip1233@users.noreply.github.com>
2023-12-03 09:01:52 +0800

    Delete Todo:gpBuild.py

--- Todo:gpBuild.py
+++
@@ -1,5 +0,0 @@
-# GP builder: it builds a GP model given 1. a kernel function, 2. a mean function, 3. a likelihood function, 4. training method. 
-# It provide data normalization and de-normalization. It also save all data and training results. It is recommend to used for testing GP but not for practical use, particularly when the data is large.
-#
-
-
