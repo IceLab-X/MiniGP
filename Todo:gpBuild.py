eec00461f588ab08ee71ee8a4d1ac689f33a1cee
Wei Xing <wayne.xingle@gmail.com>
2023-12-05 03:51:20 +0800

    Revert "Add hogp.py and hogp.ipynb"

---
+++ Todo:gpBuild.py
@@ -0,0 +1,5 @@
+# GP builder: it builds a GP model given 1. a kernel function, 2. a mean function, 3. a likelihood function, 4. training method. 
+# It provide data normalization and de-normalization. It also save all data and training results. It is recommend to used for testing GP but not for practical use, particularly when the data is large.
+#
+
+
