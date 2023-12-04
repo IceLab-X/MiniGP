5d90ee7154ad6b26c78efd664c29ed4307016783
wayxing <wayne.xingle@gmail.com>
2023-12-04 16:08:04 +0000

    fix bugs in invKernel

--- gp_basic.py
+++ gp_basic.py
@@ -1,11 +1,16 @@
+# A simple GP implementation using the modular kernel
+# Author: Wei W. Xing (wxing.me)
+# Email: wayne.xingle@gmail.com
+# Date: 2023-11-26
+
 import numpy as np
 import torch
 import torch.nn as nn
-# import kernel as kernel
-import kernel_v2 as kernel
+import kernel as kernel
+import time as time
 
 
-class GP_simple(nn.Module):
+class GP_basic(nn.Module):
     def __init__(self, kernel, noise_variance):
         super().__init__()
         self.kernel = kernel
@@ -16,7 +21,7 @@ class GP_simple(nn.Module):
         K_s = self.kernel(x_train, x_test)
         K_ss = self.kernel(x_test, x_test)
         
-        Kinv_method = 'cholesky1'    # 'direct' or 'cholesky'
+        Kinv_method = 'cholesky2'    # 'direct' or 'cholesky'
         if Kinv_method == 'cholesky1':   # kernel inverse is not stable, use cholesky decomposition instead
             L = torch.cholesky(K)
             L_inv = torch.inverse(L)
@@ -29,7 +34,8 @@ class GP_simple(nn.Module):
             L = torch.cholesky(K)
             alpha = torch.cholesky_solve(y_train, L)
             mu = K_s.T @ alpha
-            v = torch.cholesky_solve(K_s, L)
+            # v = torch.cholesky_solve(K_s, L)    # wrong implementation
+            v = L.inverse() @ K_s   # correct implementation
             cov = K_ss - v.T @ v
         elif Kinv_method == 'direct':
             K_inv = torch.inverse(K)
@@ -42,7 +48,7 @@ class GP_simple(nn.Module):
     def log_likelihood(self, x_train, y_train, use_cholesky=False):
         K = self.kernel(x_train, x_train) + self.noise_variance.pow(2) * torch.eye(len(x_train))
         
-        Kinv_method = 'cholesky1'
+        Kinv_method = 'cholesky3'
         if Kinv_method == 'cholesky1':
             L = torch.cholesky(K)
             L_inv = torch.inverse(L)
@@ -51,14 +57,18 @@ class GP_simple(nn.Module):
         elif Kinv_method == 'cholesky2':
             L = torch.cholesky(K)
             return -0.5 * (y_train.T @ torch.cholesky_solve(y_train, L) + torch.logdet(K) + len(x_train) * np.log(2 * np.pi))
+        elif Kinv_method == 'cholesky3':
+            # fastest implementation so far
+            L = torch.cholesky(K)
+            return -0.5 * (y_train.T @ torch.cholesky_solve(y_train, L) + L.diag().log().sum() + len(x_train) * np.log(2 * np.pi))
         elif Kinv_method == 'direct':
             K_inv = torch.inverse(K)
             return -0.5 * (y_train.T @ K_inv @ y_train + torch.logdet(K) + len(x_train) * np.log(2 * np.pi))
         elif Kinv_method == 'torch_distribution_MN1':
             L = torch.cholesky(K)
-            return -torch.distributions.MultivariateNormal(y_train, scale_tril=L).log_prob(y_train)
+            return torch.distributions.MultivariateNormal(y_train, scale_tril=L).log_prob(y_train)
         elif Kinv_method == 'torch_distribution_MN2':
-            return -torch.distributions.MultivariateNormal(y_train, K).log_prob(y_train)
+            return torch.distributions.MultivariateNormal(y_train, K).log_prob(y_train)
         else:
             raise ValueError('Kinv_method should be either direct or cholesky')
         
@@ -82,14 +92,19 @@ if __name__ == '__main__':
     
     # kernel1 = kernel.SumKernel(kernel.LinearKernel(1), kernel.MaternKernel(1))
     
-    GPmodel = GP_simple(kernel=kernel1, noise_variance=1.0)
+    GPmodel = GP_basic(kernel=kernel1, noise_variance=1.0)
     optimizer = torch.optim.Adam(GPmodel.parameters(), lr=1e-1)
-    for i in range(3000):
+    
+    
+    for i in range(1000):
+        startTime = time.time()
         optimizer.zero_grad()
         loss = -GPmodel.log_likelihood(xtr, ytr)
         loss.backward()
         optimizer.step()
         print('iter', i, 'nll:{:.5f}'.format(loss.item()))
+        timeElapsed = time.time() - startTime
+        print('time elapsed: {:.3f}s'.format(timeElapsed))
         
     with torch.no_grad():
         ypred, ypred_var = GPmodel.forward(xtr, ytr, xte)
--- kernel.py
+++ kernel.py
@@ -13,13 +13,29 @@
 # 2. length_scales = torch.exp(self.length_scales) + self.eps. Good for input grow exponentially.
 # 3. length_scales = F.softplus(self.raw_length_scales) where F is torch.nn.functional.
 # I choose No.1 for now by hoping that the data will be processed to be close the stationary and normal distribution.
-
+# 
 
 import torch
 import torch.nn as nn
+
 EPS = 1e-9
 
 class LinearKernel(nn.Module):
+    """
+    Linear kernel module.
+
+    Args:
+        input_dim (int): The input dimension.
+        initial_length_scale (float): The initial length scale value. Default is 1.0.
+        initial_signal_variance (float): The initial signal variance value. Default is 1.0.
+
+    Attributes:
+        length_scales (nn.Parameter): The length scales for each dimension.
+        signal_variance (nn.Parameter): The signal variance.
+        center (nn.Parameter): The center.
+
+    """
+
     def __init__(self, input_dim, initial_length_scale=1.0, initial_signal_variance=1.0):
         super(LinearKernel, self).__init__()
         self.length_scales = nn.Parameter(torch.ones(input_dim) * initial_length_scale)
@@ -27,24 +43,59 @@ class LinearKernel(nn.Module):
         self.center = nn.Parameter(torch.zeros(input_dim))
         
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the linear kernel.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         x1 = x1 / self.length_scales - self.center
         x2 = x2 / self.length_scales - self.center
         
         return x1 @ x2.T * self.signal_variance.abs()
 
 class ARDKernel(nn.Module):
+    """
+    ARD (Automatic Relevance Determination) kernel module.
+
+    Args:
+        input_dim (int): The input dimension.
+        initial_length_scale (float): The initial length scale value. Default is 1.0.
+        initial_signal_variance (float): The initial signal variance value. Default is 1.0.
+        eps (float): A small constant to prevent division by zero. Default is 1e-9.
+
+    Attributes:
+        length_scales (nn.Parameter): The length scales for each dimension.
+        signal_variance (nn.Parameter): The signal variance.
+        eps (float): A small constant to prevent division by zero.
+
+    """
+
     def __init__(self, input_dim, initial_length_scale=1.0, initial_signal_variance=1.0, eps=EPS):
         super().__init__()
-        # Initialize length scales for each dimension
         self.length_scales = nn.Parameter(torch.ones(input_dim) * initial_length_scale)
         self.signal_variance = nn.Parameter(torch.tensor([initial_signal_variance]))
-        self.eps = eps  # Small constant to prevent division by zero
+        self.eps = eps
 
     def forward(self, x1, x2):
-        # Ensure length scales are positive and add a small constant for numerical stability
+        """
+        Compute the covariance matrix using the ARD kernel.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         length_scales = torch.abs(self.length_scales) + self.eps
 
-        # Compute scaled squared Euclidean distances
         scaled_x1 = x1 / length_scales
         scaled_x2 = x2 / length_scales
         sqdist = torch.cdist(scaled_x1, scaled_x2, p=2)**2
@@ -53,18 +104,45 @@ class ARDKernel(nn.Module):
     
 # Matern kernel with independent length scales
 class MaternKernel(nn.Module):
+    """
+    Matern kernel module with independent length scales.
+
+    Args:
+        input_dim (int): The input dimension.
+        initial_length_scale (float): The initial length scale value. Default is 1.0.
+        initial_signal_variance (float): The initial signal variance value. Default is 1.0.
+        nu (float): The smoothness parameter. Default is 2.5.
+        eps (float): A small constant to prevent division by zero. Default is 1e-9.
+
+    Attributes:
+        length_scales (nn.Parameter): The length scales for each dimension.
+        signal_variance (nn.Parameter): The signal variance.
+        nu (nn.Parameter): The smoothness parameter.
+        eps (float): A small constant to prevent division by zero.
+
+    """
+
     def __init__(self, input_dim, initial_length_scale=1.0, initial_signal_variance=1.0, nu=2.5, eps=EPS):
         super().__init__()
-        # Initialize length scales for each dimension
         self.length_scales = nn.Parameter(torch.ones(input_dim) * initial_length_scale)
         self.signal_variance = nn.Parameter(torch.tensor([initial_signal_variance]))
         self.nu = nn.Parameter(torch.tensor([nu]))
         self.eps = eps
+
     def forward(self, x1, x2):
-        # Ensure length scales are positive and add a small constant for numerical stability
+        """
+        Compute the covariance matrix using the Matern kernel.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         length_scales = torch.abs(self.length_scales) + self.eps
 
-        # Compute scaled squared Euclidean distances
         scaled_x1 = x1 / length_scales
         scaled_x2 = x2 / length_scales
         sqdist = torch.cdist(scaled_x1, scaled_x2, p=2)**2
@@ -73,36 +151,123 @@ class MaternKernel(nn.Module):
 
 # kernel operations
 class SumKernel(nn.Module):
+    """
+    Sum of two kernels module.
+
+    Args:
+        kernel1 (nn.Module): The first kernel.
+        kernel2 (nn.Module): The second kernel.
+
+    Attributes:
+        kernel1 (nn.Module): The first kernel.
+        kernel2 (nn.Module): The second kernel.
+
+    """
+
     def __init__(self, kernel1, kernel2):
         super().__init__()
         self.kernel1 = kernel1
         self.kernel2 = kernel2
 
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the sum of two kernels.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         return self.kernel1(x1, x2) + self.kernel2(x1, x2)
 
 class ProductKernel(nn.Module):
+    """
+    Product of two kernels module.
+
+    Args:
+        kernel1 (nn.Module): The first kernel.
+        kernel2 (nn.Module): The second kernel.
+
+    Attributes:
+        kernel1 (nn.Module): The first kernel.
+        kernel2 (nn.Module): The second kernel.
+
+    """
+
     def __init__(self, kernel1, kernel2):
         super().__init__()
         self.kernel1 = kernel1
         self.kernel2 = kernel2
 
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the product of two kernels.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         return self.kernel1(x1, x2) * self.kernel2(x1, x2)
 
 # deprecated kernels where length_scale is a scalar
 class SquaredExponentialKernel(nn.Module):
+    """
+    Squared Exponential kernel module with scalar length scale.
+
+    Args:
+        length_scale (float): The length scale value. Default is 1.0.
+        signal_variance (float): The signal variance value. Default is 1.0.
+
+    Attributes:
+        length_scale (nn.Parameter): The length scale.
+        signal_variance (nn.Parameter): The signal variance.
+
+    """
+
     def __init__(self, length_scale=1.0, signal_variance=1.0):
         super().__init__()
         self.length_scale = nn.Parameter(torch.tensor([length_scale]))
         self.signal_variance = nn.Parameter(torch.tensor([signal_variance]))
 
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the squared exponential kernel.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         sqdist = torch.sum(x1**2, 1).reshape(-1, 1) + torch.sum(x2**2, 1) - 2 * torch.matmul(x1, x2.T)
         return self.signal_variance.pow(2) * torch.exp(-0.5 * sqdist / self.length_scale.pow(2))
     
     # need to check if this is correct
 class RationalQuadraticKernel(nn.Module):
+    """
+    Rational Quadratic kernel module with scalar length scale.
+
+    Args:
+        length_scale (float): The length scale value. Default is 1.0.
+        signal_variance (float): The signal variance value. Default is 1.0.
+        alpha (float): The alpha value. Default is 1.0.
+
+    Attributes:
+        length_scale (nn.Parameter): The length scale.
+        signal_variance (nn.Parameter): The signal variance.
+        alpha (nn.Parameter): The alpha value.
+
+    """
+
     def __init__(self, length_scale=1., signal_variance=1., alpha=1.):
         super(RationalQuadraticKernel, self).__init__()
         self.length_scale = nn.Parameter(torch.tensor([length_scale]))
@@ -110,10 +275,36 @@ class RationalQuadraticKernel(nn.Module):
         self.alpha = nn.Parameter(torch.tensor([alpha]))
 
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the rational quadratic kernel.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         sqdist = torch.sum(x1**2, 1).reshape(-1, 1) + torch.sum(x2**2, 1) - 2 * torch.matmul(x1, x2.T)
         return self.signal_variance.pow(2) * torch.pow(1 + 0.5 * sqdist / self.alpha / self.length_scale.pow(2), -self.alpha)
   
 class MaternKernel_scalarLengthScale(nn.Module):
+    """
+    Matern kernel module with scalar length scale.
+
+    Args:
+        length_scale (float): The length scale value. Default is 1.0.
+        signal_variance (float): The signal variance value. Default is 1.0.
+        nu (float): The smoothness parameter. Default is 2.5.
+
+    Attributes:
+        length_scale (nn.Parameter): The length scale.
+        signal_variance (nn.Parameter): The signal variance.
+        nu (nn.Parameter): The smoothness parameter.
+
+    """
+
     def __init__(self, length_scale=1.0, signal_variance=1.0, nu=2.5):
         super().__init__()
         self.length_scale = nn.Parameter(torch.tensor([length_scale]))
@@ -121,6 +312,17 @@ class MaternKernel_scalarLengthScale(nn.Module):
         self.nu = nn.Parameter(torch.tensor([nu]))
 
     def forward(self, x1, x2):
+        """
+        Compute the covariance matrix using the Matern kernel with scalar length scale.
+
+        Args:
+            x1 (torch.Tensor): The first input tensor.
+            x2 (torch.Tensor): The second input tensor.
+
+        Returns:
+            torch.Tensor: The covariance matrix.
+
+        """
         sqdist = torch.sum(x1**2, 1).reshape(-1, 1) + torch.sum(x2**2, 1) - 2 * torch.matmul(x1, x2.T)
         return self.signal_variance.pow(2) * torch.pow(1 + torch.sqrt(3 * sqdist) / self.length_scale.pow(2), -self.nu)
 
