7258e719474911fa356e281b6dd4aac8cafa0692
Wei Xing <wayne.xingle@gmail.com>
2023-12-20 01:06:10 +0000

    Remove unused files

--- Todo_gp_deepKernel.py
+++
@@ -1,8 +0,0 @@
-# A simple GP implementation using the modular kernel with deep kernel learning (DKL) Ref Paper: https://arxiv.org/abs/1511.02222
-# Author: Wei W. Xing (wxing.me)
-# Email: wayne.xingle@gmail.com
-# Date: 2023-12-1
-
-
-
-
